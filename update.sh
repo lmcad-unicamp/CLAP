ebb24e1d3a9baf8dc4c6f96bee65ed2c0431a478
Otavio Napoli <otavio.napoli@gmail.com>
2020-03-04 18:15:49 -0300

    New update file

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CLAP_PATH=~/.clap
+
+echo -n "Pulling repository for updates... "
+date
+git pull
+echo "OK"
+
+echo "Using default CLAP path: $CLAP_PATH"
+
+echo -n "Copying new group files... "
+cp -r ./share/groups/ $CLAP_PATH
+echo "OK"
+
+echo -n "Copying new module files... "
+cp -r ./share/modules/ $CLAP_PATH
+echo "OK"
