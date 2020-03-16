3be0b9fef29f1087cf9ba5f52ce8582189201cdd
Otavio Napoli <otavio.napoli@gmail.com>
2020-03-16 19:34:37 -0300

    Generated docs

--- app/server/app.py
+++ app/server/app.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, flash, Markup
+from flask import Flask, render_template, request, json
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, PasswordField, IntegerField, TextField, FormField, \
     SelectField, FieldList, TextAreaField
 from wtforms.validators import DataRequired, Length
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-import json
 
-from clap.common.factory import PlatformFactory
-from .commands import *
-
+from app.server import commands
 
 app = Flask(__name__)
 app.secret_key = 'dev'
@@ -19,11 +16,16 @@ bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 
 
+@app.route('/get-nodes', methods=['GET', 'POST'])
+def get_nodes(result=None):
+    nodes = json.dumps([node.__dict__ for node in commands.node_get()])
+    return nodes
+    #nodes = [node.__dict__ for node in commands.node_get()]
+    #return render_template('nodes-div.html', nodes=nodes)
+
 @app.route('/', methods=['GET', 'POST'])
 def index(result=None):
-    nodes = json.dumps([node.__dict__ for node in node_get()])
-    print(nodes)
-    return render_template('index.html', nodes=nodes)
+    return render_template('index.html')
 
 
 def process_request(text):
