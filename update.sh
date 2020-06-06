aa59144e8a0647b3681a05193b8abb6c44e6a3d2
Otavio Napoli <otavio.napoli@gmail.com>
2020-06-06 00:39:43 -0300

    Changes in install.sh and update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+function green_print(){
+  printf "${GREEN}$1${NC}\n"
+}
+
+function abort() {
+	echo "Error: $1"
+	exit 1
+}
+
 CLAP_PATH=~/.clap
 
 echo -n "Pulling repository for updates... "
 date
+git checkout master
 git pull
-git checkout .
 echo "OK"
 
-echo "Using default CLAP path: $CLAP_PATH"
+source clap-env/bin/activate || abort "Activating venv"
+green_print "Updating clap...."
+pip --no-cache-dir install . || abort "Updating clap..."
+green_print "OK"
 
-./install.sh
+green_print "Updating CLAP path: $CLAP_PATH"
+cp -r -n ./share/configs/ $CLAP_PATH
+cp -r ./share/groups/ $CLAP_PATH
+cp -r ./share/modules/ $CLAP_PATH
+green_print "OK"
