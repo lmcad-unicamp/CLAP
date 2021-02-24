dd4272e3251ab3e132ee351dd6fc1b3c236b16a5
gsribeiro <gabriel4k2@gmail.com>
2021-02-24 14:38:33 -0300

    fixing update

--- update.sh
+++ update.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 GREEN='\033[0;32m'
 NC='\033[0m'
 
@@ -14,16 +12,16 @@ function abort() {
 
 CLAP_PATH=~/.clap
 
-#echo -n "Pulling repository for updates... "
-#date
-#git checkout master
-#git pull
-#echo "OK"
+echo -n "Pulling repository for updates... "
+date
+git checkout master
+git pull
+echo "OK"
 
 source clap-env/bin/activate || abort "Activating venv"
-#green_print "Updating clap...."
-#pip --no-cache-dir install . || abort "Updating clap..."
-#green_print "OK"
+green_print "Updating clap...."
+pip --no-cache-dir install . || abort "Updating clap..."
+green_print "OK"
 
 green_print "Updating CLAP path: $CLAP_PATH"
 cp -r -n ./share/configs/ $CLAP_PATH
