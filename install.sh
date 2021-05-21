626d68fa22bf0ad5a64068a0b64ea95d2b57a9dc
Otavio Napoli <>
2021-05-21 01:46:40 -0300

    Added share files and updated install.sh

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ function abort() {
 
 green_print "Creating clap's virtualenv..."
 python3 -m venv clap-env || abort "Creating venv"
-echo "export CLAP=$PWD" >> clap-env/bin/activate || abort "Error writting on activate"
-echo "export CLAP_PATH=$CLAP_PATH" >> clap-env/bin/activate || abort "Error writting on activate"
-echo "export PATH=\$PATH:$PWD" >> clap-env/bin/activate || abort "Error writting on activate"
+echo "export CLAP=$PWD" >> clap-env/bin/activate || abort "Error writing on activate"
+echo "export CLAP_PATH=$CLAP_PATH" >> clap-env/bin/activate || abort "Error writing on activate"
+echo "export PATH=\$PATH:$PWD" >> clap-env/bin/activate || abort "Error writing on activate"
 source clap-env/bin/activate || abort "Activating venv"
 green_print "OK"
 
@@ -32,15 +32,15 @@ green_print "OK"
 
 # echo 'eval "$(register-python-argcomplete clapp)"' >> clap-env/bin/activate || abort "Error writting on activate"
 
-#green_print "Creating default folders"
-#green_print "Using default CLAP path: $CLAP_PATH"
-#mkdir -p $CLAP_PATH/configs/
-#mkdir -p $CLAP_PATH/configs/clusters/
-#mkdir -p $CLAP_PATH/groups/
-#mkdir -p $CLAP_PATH/private/
-#mkdir -p $CLAP_PATH/storage/
-#cp -r -n ./share/configs/ $CLAP_PATH
-#cp -r ./share/groups/ $CLAP_PATH
+green_print "Creating default folders"
+green_print "Using default CLAP path: $CLAP_PATH"
+mkdir -p $CLAP_PATH/configs/
+mkdir -p $CLAP_PATH/configs/clusters/
+mkdir -p $CLAP_PATH/roles/
+mkdir -p $CLAP_PATH/private/
+mkdir -p $CLAP_PATH/storage/
+cp -r -n ./share/configs/ $CLAP_PATH
+cp -r ./share/roles/ $CLAP_PATH
 
 green_print "OK"
 green_print "Use: 'source ./clap-env/bin/activate' to use CLAP"
